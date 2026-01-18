2de5bfecf1b84d58bd059ed5ccbd07884bd6a197
dbstjd2ek <dbstjd2ek@gmail.com>
2026-01-18 19:20:04 +0900

    unsigned_integer.c

---
+++ unsigned_integer.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+int main()
+{
+unsigned char num1 = 200; // 1바이트 부호 없는 정수형으로 변수를 선언하고 값 할당
+
+unsigned short num1 = 60000; // 2바이트 부호 없는 정수형으로 변수를 선언하고 값 할당
+
+unsigned int num3 = 4123456789; // 4바이트 부호 없는 정수형으로 변수를 선언하고 값 할당
+
+unsigned long num4 = 4123456789; // 4바이트 부호 없는 정수형으로 변수를 선언하고 값 할당
+return 0;
+}
